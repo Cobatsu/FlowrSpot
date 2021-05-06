90980c66f208c17feccbfe9527e25ab5619d7ebb
Cobatsu <huze.ozr@gmail.com>
2021-05-07 00:44:44 +0200

    completed the login fetch process

--- src/Components/modals/login.js
+++ src/Components/modals/login.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Button } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 import { useFormik } from 'formik';
+import {validate} from 'email-validator'
 
 const ModalContainer = styled.div`
 
@@ -32,6 +33,8 @@ const SubmitButton = styled.button`
     box-shadow: 0px 15px 20px rgba(234, 168, 159, 0.2);
     border-radius: 3px;
     color:#FFFFFF;
+    font-weight: 500;
+    font-size: 14px;
     padding:22px;
     &:hover{
         cursor:pointer;
@@ -40,6 +43,7 @@ const SubmitButton = styled.button`
 `
 const Title = styled.span`
     font-weight:500;
+    font-size:20px;
     text-align:center;
 `
 const Fields = styled.div`
@@ -54,21 +58,52 @@ const Form = styled.form`
     height:100%;
     width:100%;
 `
+const validateStageValues = values => {
 
-const LoginModal = ({isTouched})=>{
+    let errorStore = {}; 
+    let { email , password } = values; 
 
+    if (!email) {
 
-    const formik = useFormik({
+        errorStore.email = "This Field Can Not Be Blank!"
+
+    } else if (!validate(email)) { // " Email-validator is a small library that helps to validate user's email
+
+        errorStore.firstName = 'Wrong Format !';
+
+    }
+  
+    if (!password) {
+
+        errorStore.email = "This Field Can Not Be Blank!"
+
+    } 
+
+}
+
+const LoginModal = (props)=>{
+
+    const formik = useFormik({ // Here, We dont need to "DECLARE" and "HANDLE" our own state to store data  because useFormik already does it for us ! .
+
         initialValues:{
             email:'',
             password:''
         },
-        onSubmit:values => {
+        onSubmit:async (stagedData) => {
 
-        }
+            const res = await fetch("https://flowrspot-api.herokuapp.com/api/v1/users/login",{
+                method:"POST",
+                body:stagedData
+            });
+
+            const { error , token } = await res.json();
+
+            if(error) {
+
+            }
+        },
+        validate:validateStageValues
     })
-
-
 
     return <ModalContainer>
 
@@ -78,7 +113,7 @@ const LoginModal = ({isTouched})=>{
 
             <Fields>
 
-                <TextField
+                <TextField 
                     style={{marginTop:8}} 
                     label="Email Address" 
                     variant="outlined"
@@ -86,6 +121,8 @@ const LoginModal = ({isTouched})=>{
                     type="email"
                     onChange={formik.handleChange} 
                     value={formik.values.email} />
+
+                { /* It is great fun to use customized inputs provided by material UI ! */}
 
                 <TextField  
                     style={{marginTop:8}}   
