59566c428ef636d9d05f0586369f00512ca6fc8d
Cobatsu <huze.ozr@gmail.com>
2021-05-06 22:57:41 +0200

    Fixed BackStage Styling

--- src/Components/modals/index.js
+++ src/Components/modals/index.js
@@ -1,45 +1,53 @@
 import Login from './login'
 import Logout from './logout'
 import Register from './register'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 import React from 'react'
 import styled from 'styled-components'
 
 const BackStage = styled.div`
 
     position:fixed;
+    width:100%;
     top:0;
     left:0;
-    width:100%:
-    height:100%;   
+    height:100vh;
     background-color:black;
-    opacity:0.35;
-
+    opacity:0.3;
+    z-index:1000;
 
 `
 
 const PopUp = ()=>{
 
     const type  = useSelector((store)=>store.modal.modalType)
+    const dispatch = useDispatch();
 
     let ActiveModal = null;
+
+    const closeModal = ()=>{
+        dispatch({
+            type:null
+        })
+    }
 
     switch(type) {
 
         case "Login":
             ActiveModal = <React.Fragment>  
-                            <BackStage/> 
+                            <BackStage onClick={closeModal}/> 
                             <Login/> 
                           </React.Fragment> 
+            break;
         case "Logout":
             ActiveModal = <React.Fragment>  
-                             <BackStage/> 
+                             <BackStage onClick={closeModal}/> 
                              <Logout/> 
                           </React.Fragment> 
             break;
         case "Register":
             ActiveModal = <React.Fragment>  
-                             <BackStage/> 
+                             <BackStage onClick={closeModal}/> 
                              <Register/> 
                          </React.Fragment> 
             break;    
--- src/Components/modals/login.js
+++ src/Components/modals/login.js
@@ -3,22 +3,31 @@ import styled from 'styled-components';
 
 const ModalContainer = styled.div`
 
-    position: absolute;
-    left: 0%;
-    right: 0%;
-    top: 0%;
-    bottom: 10.22%;
-
-    background: #FFFFFF;
-    box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.05);
-    border-radius: 3px;
-
+    position:fixed;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    flex-flow:column;
+    width: 440px;
+    height: 290px;
+    top: 50%;
+    left: 50%;
+    margin-top: -20px;
+    margin-left: -250px;
+    border-radius:8px;
+    padding:15px;
+    box-sizing:border-box;
+    top:25%;
+    background:#FFFFFF;
+    box-shadow: 0 1px 6px -1px rgba(0, 0, 0,0.4), 0 2px 4px -1px rgba(0, 0, 0, 0.02);
+    z-index:1001;
 `
 
 const LoginModal = ({isTouched})=>{
 
-    return <h1>Login</h1>
+    return <ModalContainer/>
 
 }
 
 export default LoginModal;
+
